0fadd891afc74e0fe513c2659c08256f39ce7135
jayliu9 <jayliusj@gmail.com>
2023-12-23 00:51:20 -0800

    health component & damage

--- scenes/ability/sword_ability_controller/sword_ability_controller.gd
+++ scenes/ability/sword_ability_controller/sword_ability_controller.gd
@@ -29,9 +29,9 @@ func on_timer_timeout():
 		return a_distance < b_distance
 	)
 	
-	var sword_instance = sword_ability.instantiate() as Node2D
+	var sword_instance = sword_ability.instantiate() as SwordAbility
+	player.get_parent().add_child(sword_instance)
+	sword_instance.hitbox_component.damage = 5
 	sword_instance.global_position = enemies[0].global_position + Vector2.RIGHT.rotated(randf_range(0, TAU)) * 4
 	var enemy_direction = enemies[0].global_position - sword_instance.global_position
 	sword_instance.rotation = enemy_direction.angle()
-	player.get_parent().add_child(sword_instance)
-	
--- scenes/game_object/basic_enemy/basic_enemy.gd
+++ scenes/game_object/basic_enemy/basic_enemy.gd
@@ -1,10 +1,7 @@
 extends CharacterBody2D
 
+@onready var health_component: HealthComponent = $HealthComponent
 const MAX_SPEED = 50
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	$Area2D.area_entered.connect(on_area_entered)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -20,6 +17,3 @@ func get_direction_to_player():
 		return (player.global_position - global_position).normalized()
 	return Vector2.ZERO
 
-
-func on_area_entered(area: Area2D):
-	queue_free()
