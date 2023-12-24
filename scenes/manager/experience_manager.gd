747e43a2487fcf00c0a3a9ff300b48fac3fd5e35
jayliu9 <jayliusj@gmail.com>
2023-12-24 00:37:07 -0800

    experience bar

--- scenes/manager/experience_manager.gd
+++ scenes/manager/experience_manager.gd
@@ -1,6 +1,11 @@
 extends Node
 
+signal experience_update(current_experience: float, target_experience: float)
+
+const TARGET_EXPERIENCE_GROWTH = 5
 var current_experience = 0
+var current_level = 1
+var target_experience = 5
 
 
 func _ready():
@@ -12,5 +17,11 @@ func on_exp_vial_collected(number: float):
 
 
 func increment_experience(number: float):
-	current_experience += number
-	print(current_experience)
+	current_experience = min(current_experience + number, target_experience)
+	experience_update.emit(current_experience, target_experience)
+	
+	if current_experience == target_experience:
+		current_level += 1
+		target_experience += TARGET_EXPERIENCE_GROWTH
+		current_experience = 0
+		experience_update.emit(current_experience, target_experience)
