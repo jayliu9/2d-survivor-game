ea8dd97bd370c4a918667a6e1ef8f5c118a85657
jayliu9 <jayliusj@gmail.com>
2023-12-27 01:16:45 -0800

    upgrade resource & ui

--- scenes/manager/experience_manager.gd
+++ scenes/manager/experience_manager.gd
@@ -1,11 +1,12 @@
 extends Node
 
 signal experience_update(current_experience: float, target_experience: float)
+signal level_up(new_level: int)
 
 const TARGET_EXPERIENCE_GROWTH = 5
 var current_experience = 0
 var current_level = 1
-var target_experience = 5
+var target_experience = 3
 
 
 func _ready():
@@ -25,3 +26,4 @@ func increment_experience(number: float):
 		target_experience += TARGET_EXPERIENCE_GROWTH
 		current_experience = 0
 		experience_update.emit(current_experience, target_experience)
+		level_up.emit(current_level)
